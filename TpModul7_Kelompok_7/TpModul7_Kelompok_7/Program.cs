594f203568f37d948139fb3041b2b51ae6f7e104
FixSann <sanhakim536@gmail.com>
2025-04-14 17:11:52 +0700

    git pull dan merge ke mainikhsan

--- TpModul7_Kelompok_7/TpModul7_Kelompok_7/Program.cs
+++ TpModul7_Kelompok_7/TpModul7_Kelompok_7/Program.cs
@@ -12,6 +12,11 @@ namespace TpModul7_Kelompok_7
             Console.WriteLine("=== PROGRAM DESERIALISASI JSON ===");
             Console.WriteLine("=== Praktikum Modul 7 - NIM 2311104064 ===\n");
 
+            // Jalankan fungsi pertama (Data Mahasiswa)
+            Console.WriteLine("\n===============MENAMBAHKAN JSON DESERIALIZATON 1==================");
+            Console.WriteLine("\n[1] MEMBACA DATA MAHASISWA");
+            DataMahasiswa2311104064.ReadJSON();
+
             // Jalankan fungsi kedua (Daftar Mata Kuliah)
             Console.WriteLine("\n==================MENAMBAHKAN JSON DESERIALIZATON 2=====================");
             Console.WriteLine("\n[2] MEMBACA DAFTAR MATA KULIAH");
